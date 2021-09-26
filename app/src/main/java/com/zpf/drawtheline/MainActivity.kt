6221e6508f362310303ee6853bacc854f4efd5cc
ZhaoPengFei <2722246227@qq.com>
2021-09-26 10:18:53 +0800

    完成滑动

--- app/src/main/java/com/zpf/drawtheline/MainActivity.kt
+++ app/src/main/java/com/zpf/drawtheline/MainActivity.kt
@@ -2,12 +2,14 @@ package com.zpf.drawtheline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.hide()
 
 
     }
